7fbe9e889dc02ecf4a6297c694c767bffe434cfd
vahnatai <vahnatai@users.noreply.github.com>
2022-07-15 01:20:46 -0230

    use virtual text input to control caret display

--- js/index.js
+++ js/index.js
@@ -3,10 +3,13 @@ import GameConsole from './GameConsole.js';
 window.onload = () => {
 	const outputBox = document.getElementById('outputBox');
 	const inputBox = document.getElementById('inputBox');
+	const preCaret = document.getElementById('preCaret');
+	const virtualCaret = document.getElementById('virtualCaret');
+	const postCaret = document.getElementById('postCaret');
 	const sendButton = document.getElementById('sendButton');
 	const pointCount = document.getElementById('pointCount');
 	const stepCount = document.getElementById('stepCount');
 
-	const gc = new GameConsole(inputBox, outputBox, pointCount, stepCount, sendButton);
+	const gc = new GameConsole(inputBox, outputBox, preCaret, virtualCaret, postCaret, pointCount, stepCount, sendButton);
 	gc.start();
 };
